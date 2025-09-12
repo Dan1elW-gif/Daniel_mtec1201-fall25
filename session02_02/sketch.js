31679cacd27464670ba480e2a1ac4d2dc6a38275
Dan1elW-gif <yanchakdanielw@gmail.com>
2025-09-12 11:33:31 -0400

    Add files via upload

--- session02_02/sketch.js
+++ session02_02/sketch.js
@@ -6,7 +6,7 @@ function setup() {
   background(20,100,220);
 
   //sun
-  fill(255,200,0);
+  fill(255,300,0);
   ellipse(200,100,100,100)
 
   //ground
@@ -17,6 +17,18 @@ function setup() {
   fill(0,200,200,127);
   rect(550, 450, 200, 100);
   
+  //head
+  fill(255, 220, 180);
+  ellipse(550, 350, 80, 80);
+  
+  //face
+  fill(0);
+  ellipse(535, 345, 10, 10);
+  ellipse(565, 345, 10, 10);
+  
+  arc(550, 360, 40, 20, 20)
+
+
 }
 
 
