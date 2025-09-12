6c40a7765792f652818969197f92b1616e971bb0
Dan1elW-gif <yanchakdanielw@gmail.com>
2025-09-12 12:47:28 -0400

    Update sketch.js

--- s0301/sketch.js
+++ s0301/sketch.js
@@ -1,7 +1,8 @@
 function setup() 
 {
   createCanvas(800,600);
-
+  strokeWeight(8)
+  //background(125,127,50);
 }
 
 function draw() { //line code in draw block run block
@@ -11,4 +12,7 @@ function draw() { //line code in draw block run block
 
   //a DYNAMIC LINE
   line(400,300, mouseX, mouseY)
+  fill(0,255,0)  
+  //ellipse following mouse  
+  ellipse(mouseX, mouseY,50,50)
 }
